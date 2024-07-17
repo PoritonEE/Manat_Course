63db8aac2094e9300886545532cc0a332e1d2a5b
Danel-Var <danelvar@gmail.com>
2024-07-17 19:02:41 +0300

    progres- it's works with some bugs

--- HW4/grades.c
+++ HW4/grades.c
@@ -19,8 +19,18 @@ typedef struct student{
 }Students;
 
 struct grades{
-    struct list* studnet_list;    
+    struct list* student_list;    
 };
+
+//deaclerations
+
+int add_grade(struct student* student,  int grade,const char* name);
+/**
+ * @brief helps printing student
+ * @param student - will print the given object
+ * @return 0 on success
+ */
+int student_print(struct student* student);
 
 //---------courses------------
 
@@ -98,9 +108,9 @@ struct grades* grades_init(){
         return NULL;
     }
 
-    init_grades->studnet_list= list_init(&clone_student,
+    init_grades->student_list= list_init(&clone_student,
                                         &destroy_student);
-    if(!init_grades->studnet_list){
+    if(!init_grades->student_list){
         return NULL;
     }    
    
@@ -111,7 +121,7 @@ void grades_destroy(struct grades *grades){
     if(grades==NULL){
         return;
     }
-    list_destroy(grades->studnet_list);
+    list_destroy(grades->student_list);
     free(grades);
 }
 
@@ -120,13 +130,175 @@ int grades_add_student(struct grades *grades, const char *name, int id){
         return FAIL;
     }
     int status;
-    Students *student;
-    student->name=name;
-    student->ID=id;
-    status=list_push_back(grades->studnet_list, student);
+    Students new_student;
+    Students *exists_student;
+    //check if the name is valid
+    if(name==NULL){
+        return FAIL;
+    }
+    // check if studnet with the same ID is already exist
+
+    for(struct iterator* it = list_begin(grades->student_list);
+        it != NULL;
+        it = list_next(it))
+    {
+        exists_student=(Students *)list_get(it);
+        if(exists_student !=NULL && exists_student->ID ==id){
+            return FAIL;
+        }
+    }
+    //create a new studnet
+    char *p_name=(char *)malloc((strlen(name)+1)*sizeof(char));
+    if(p_name== NULL){
+        return FAIL;
+    }
+    new_student.name=p_name;
+    strcpy(new_student.name,name);    
+    new_student.ID=id;
+    status=list_push_back(grades->student_list, &new_student);
+    free(p_name);
     if(status!=Success){
         return FAIL;    
     } else{
         return Success;
     }
+}
+
+int grades_add_grade(struct grades *grades,
+                     const char *name, int id, int grade){
+    
+    if(grades==NULL){
+        return 1;
+    }
+    struct iterator* curr=list_begin(grades->student_list);
+    struct student* curr_student;
+    //struct course* course;
+       
+    while(curr!=NULL){
+        curr_student=list_get(curr);
+        if(curr_student!=NULL && curr_student->ID==id){
+           return add_grade(curr_student, grade, name); 
+            //return 0;
+
+        }
+        curr=list_next(curr);
+    }
+    return 1;
+ }
+
+float grades_calc_avg(struct grades *grades, int id, char **out){
+    if(grades==NULL){
+        return -1;
+    }
+    struct iterator* curr=list_begin(grades->student_list);
+    struct student* curr_student=NULL;
+    int found=0;
+    float avg=0;
+    float count=0;
+    
+    while(curr!=NULL){
+        curr_student=list_get(curr);
+        if(curr_student->ID==id){
+            found=1;
+            break;
+        }
+        curr=list_next(curr);
+    }
+    if(!found){
+        return -1;
+    }
+    curr=list_begin(curr_student->courses);
+    struct course* curr_course;
+    while(curr!=NULL){
+        curr_course=list_get(curr);
+        if(curr_course!=NULL){
+            avg= avg+curr_course->grade;
+            count++;
+        }
+        curr=list_next(curr);
+    }
+    if(count!=0){
+        avg=avg/count;
+    }
+    else
+        return 0;
+    return avg;
+
+ }
+
+ int add_grade(struct student* student,  int grade,const char* name){
+    if(student==NULL  ||grade>100 || grade<0 || name==NULL){
+        return FAIL;
+    }
+    //struct iterator* curr=list_begin(student->courses);
+     //do we need to check if the course exists? it could appear twice ig
+    int status;
+    Course new_course;
+    new_course.grade=grade;
+    char *p_name=(char *)malloc((strlen(name)+1)*sizeof(char));
+    new_course.name=p_name;
+    strcpy(new_course.name, name);
+    status=list_push_back(student->courses, &new_course);
+    free(p_name);
+    if (status) {
+        return Success;
+    }
+
+    return FAIL;
+
+ }
+
+int student_print(struct student* student){
+    if(student==NULL){
+        return FAIL;
+    }
+    struct iterator* curr=list_begin(student->courses);
+    struct course* course;
+    printf("%s %d:", student->name, student->ID);
+    while( curr!=NULL){
+        course=list_get(curr);
+        if(course!=NULL){
+            printf(", %s %d", course->name, course->grade);
+        }
+        curr=list_next(curr);
+    }
+    printf("\n");
+    return Success;    
+}
+
+int grades_print_student(struct grades* grades, int id){
+    if(grades == NULL){
+        return FAIL;
+    }
+        struct iterator* curr=list_begin(grades->student_list);
+        struct student* curr_student;
+        int found=0;
+        while(curr!=NULL){
+            curr_student= list_get(curr);
+            if(curr_student->ID==id){
+                found=1;
+                break;
+            }
+            curr=list_next(curr);
+        }
+        if(found){
+            student_print(curr_student);
+            return Success;
+        }
+        return FAIL;      
+}
+
+int grades_print_all(struct grades *grades){
+    if(grades == NULL){
+        return FAIL;
+    }
+    struct iterator* curr= list_begin(grades->student_list);
+    struct student* curr_student;
+    while(curr!=NULL){
+        curr_student=list_get(curr);
+        student_print(curr_student);
+        curr=list_next(curr);        
+    }
+    return Success;
+
 }
